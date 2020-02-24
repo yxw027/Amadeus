053241c4918a6183e79346f75f697c58b9ed5b3d
XF <365057253@qq.com>
2020-02-24 10:22:07 +0800

    添加软件主窗口标题、图标，应用程序图标

--- Amadeus/main.cpp
+++ Amadeus/main.cpp
@@ -5,6 +5,14 @@ int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	MainWindow w;
+
+	// 设置主窗口标题
+	w.setWindowTitle("Amadeus 1.0.1");
+
+	// 设置主窗口图标
+	QIcon icon = QIcon(":/icons/Resources/MainWindowTitle.png");
+	w.setWindowIcon(icon);
+
 	w.show();
 	return a.exec();
 }
